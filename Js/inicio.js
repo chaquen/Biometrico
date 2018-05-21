38a2db9933ac184a92dd02ab2dd2538e2c291159
StalinDev <31050469+StalinDev@users.noreply.github.com>
2018-05-21 18:37:05 -0500

    Funcionaidad Eventos
    Se agregan archivos para la funionalidad de la mascar en la consulta de eventos

---
+++ Js/inicio.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#descarga').click(function(){
+        $('#ventanaDescarga').fadeIn('slow');
+    });
+    
+    $('.salir').click(function(){
+        $('.mascara').fadeOut('slow');
+    });
+});

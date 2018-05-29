f16f9cddc41fd34e9d55280f81f64c7f5fa7441e
chaquen <edgar.guzman21@gmail.com>
2018-05-28 20:26:48 -0500

    Se agrega funcionalidad para los eventos y login

---
+++ script_data/globales.js
@@ -0,0 +1,16 @@
+/*
+ *Aqui se declaran las variables que van a ser tomadas como variables globales 
+ */
+//variable que almacena el usuario
+
+var globales={
+	//_URL:"http://localhost/api_biometric/",
+	_URL:"http://biometric.mohansoft.com/",
+	
+	//_URL:"https://",
+	_usuario:false,
+	_cerrar_sesion:true,
+	_recordarme:false,
+
+};
+
---
+++ script_data/registro_usuario.js
@@ -0,0 +1,5 @@
+function iniciar_registr_usuario(){
+
+}
+
+agregarEventoLoad(iniciar_registr_usuario);

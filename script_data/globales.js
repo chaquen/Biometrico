233e10de8ad26a3c9f355e36c3c909a30766e384
StalinDev <31050469+StalinDev@users.noreply.github.com>
2018-05-29 11:46:04 -0500

    html eventos
    Formularios para el registro de pobladores y trabajadores, que participan del evento

--- script_data/globales.js
+++ script_data/globales.js
@@ -4,13 +4,13 @@
 //variable que almacena el usuario
 
 var globales={
-	//_URL:"http://localhost/api_biometric/",
-	_URL:"http://biometric.mohansoft.com/",
+	_URL:"http://localhost/api_biometric/",
+	//_URL:"http://biometric.mohansoft.com/",
 	
 	//_URL:"https://",
 	_usuario:false,
 	_cerrar_sesion:true,
-	_recordarme:false,
+	_recordarme:false
 
 };
 
